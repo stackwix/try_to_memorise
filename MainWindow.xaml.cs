c585fddfbc684b27e3fee825e9ac89fd39cedca5
Stanislav Kozlovskiy <stackwix@gmail.com>
2020-10-26 19:57:39 +0200

    Renamed project, namespace.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp
 {
 	//Нужно придумать нормальное название
 	public class MyRadioBtn : RadioButton
